ce1dd0c11b3ad307a9492336be97b29c4992c820
janevnikola <72938546+janevnikola@users.noreply.github.com>
2020-10-16 14:02:29 +0200

    Add files via upload

---
+++ Zadaca 0 - Dobredojdovte vo C programiranje.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*Da se ispecati "Dobredojdovte vo C programiranje - Zadaci"*/
+
+int main (){
+printf("Dobredojdovte vo C programiranje - Zadaci"); //pecatenje
+
+return 0;
+}
