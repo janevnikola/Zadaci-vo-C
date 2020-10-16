ff655a3427b827b6bcb4c865eb19d834393a1a7e
janevnikola <72938546+janevnikola@users.noreply.github.com>
2020-10-16 15:08:36 +0200

    Add files via upload

---
+++ Zadaca 2 -Rezultat od matematicki izraz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Да се напише програма која ќе ја пресметува вредноста на математичкиот
+//израз: x = 3/2 + (5 – 46*5/12)
+
+int main (){
+double  x = (double)3/2 + (5 - 46*(double)5/12);
+printf("Rezultat od matematickit izraz iznesuva %.2lf", x); //Delenje so int sekogas dava cel broj odnosno rezultatot
+                                                            //ke bide -13.00, a dokolku barem eden od niv e realen broj
+                                                            //togas delenjeto ke bide korektno i ke se dade tocniot rezultat
+return 0;                                                   //odnosno -12.67
+}
