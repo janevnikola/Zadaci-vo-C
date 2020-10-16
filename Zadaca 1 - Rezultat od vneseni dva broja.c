9bf417a4aaeda84c0d2849ad51b47b2a060dc221
janevnikola <72938546+janevnikola@users.noreply.github.com>
2020-10-16 14:36:52 +0200

    Add files via upload

---
+++ Zadaca 1 - Rezultat od vneseni dva broja.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Da se napise programa za sobiranje na dva brroja
+//Dvata broja se citaat od SV (Standarden Vlez)
+
+int main (){
+int broj1, broj2;
+printf("Vnesete dva broja: ");
+scanf("%d %d", &broj1, &broj2);
+printf("Rezultatot od sobiranjeto na brojot %d so brojot %d e %d", broj1, broj2, broj1+broj2);
+return 0;
+}
