7aa43c09e822671fb6ac9704bbcac29a85857c73
janevnikola <72938546+janevnikola@users.noreply.github.com>
2020-10-21 22:39:02 +0200

    Add files via upload

---
+++ Zadaca 7 - Plostina_I_Perimetar_na_Kruznica.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define PI 3.14 //so koristenje na predprocesorot
+
+//Да се напише програма за пресметување и печатење на плоштината и периметарот на круг.
+//Радиусот на кругот се чита од стандарден влез (тастатура)како децимален број
+
+int main (){
+float radius,perimetar,plostina;
+printf("Vnesi radius: ");
+scanf("%f", &radius);
+perimetar=2*radius*PI;
+printf("Perimetarot L = %.2f", perimetar);
+plostina=radius*radius*PI;
+printf("\nPlostinata P = %.2f", plostina);
+return 0;
+}
