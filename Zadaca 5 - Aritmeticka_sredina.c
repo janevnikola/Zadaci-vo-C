615e572c6ef1427e515ef05d7360ea98af1af739
janevnikola <72938546+janevnikola@users.noreply.github.com>
2020-10-21 22:07:33 +0200

    Add files via upload

---
+++ Zadaca 5 - Aritmeticka_sredina.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//Да се напише програма за пресметување на аритметичката средина на броевите 3, 5 и 12
+
+
+int main(){
+float aritmeticka_sredina;
+aritmeticka_sredina=(3+5+12)/3.0;
+printf("Aritmetickata sredina iznesuva %.2f", aritmeticka_sredina);
+
+return 0;
+}
+
