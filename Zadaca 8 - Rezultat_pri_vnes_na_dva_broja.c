9da1a66a473200e09ef59342fabd976a8a5863ee
janevnikola <72938546+janevnikola@users.noreply.github.com>
2020-10-29 16:24:33 +0100

    Add files via upload

---
+++ Zadaca 8 - Rezultat_pri_vnes_na_dva_broja.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*Да се напише програма која ќе ги изведува основните математички операции (+, -, *, /, %)
+врз два броја кои се читаат од стандарден влез
+
+Input 	Result
+
+10 12    a + b = 22
+        a - b = -2
+        a * b = 120
+        a / b = 0
+        a % b = 10
+
+
+*/
+
+int main(){
+int broj1, broj2;
+scanf("%d %d", &broj1, &broj2);
+printf("a + b = %d\n", broj1+broj2);
+printf("a - b = %d\n", broj1-broj2);
+printf("a * b = %d\n", broj1*broj2);
+printf("a / b = %d\n", broj1/broj2);
+printf("a %% b = %d", broj1%broj2);
+
+return 0;
+}
